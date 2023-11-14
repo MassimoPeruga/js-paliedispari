d4a7f944f4e8ee9c74bc3999c20506903dde57f3
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-11-14 23:23:34 +0100

    feat: parole palindrome

---
+++ palindromo/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// Chiedi all'utente di inserire una parola
+let parolaUtente;
+do {
+    parolaUtente = prompt("Inserisci una parola:");
+} while (!parolaUtente || !isParolaValida(parolaUtente));
+
+// Funzione per verificare se una parola è composta solo da lettere
+function isParolaValida(parola) {
+    // Controlla se la parola contiene solo lettere
+    return /^[a-zA-Z]+$/.test(parola);
+}
+
+// Funzione per invertire una parola 
+function invertiParola(parola) {
+    let parolaInvertita = "";
+    for (let i = parola.length - 1; i >= 0; i--) {
+        parolaInvertita += parola[i];
+    }
+    return parolaInvertita;
+}
+
+// Funzione per verificare se una parola è palindroma
+function isPalindroma(parola) {
+    // Confronta la parola originale con la parola invertita
+    return parola.toLowerCase() === invertiParola(parola.toLowerCase());
+}
+
+// Risultato
+if (isPalindroma(parolaUtente)) {
+    alert("La parola è palindroma!");
+} else {
+    alert("La parola non è palindroma.");
+}
